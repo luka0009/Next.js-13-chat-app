678105d1751ff9e36b062cb884774161f56e4e6a
luka <lukabarbakadze18@gmail.com>
2023-05-27 12:11:30 +0400

    loading ui

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { User } from "@prisma/client";
 
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/modals/LoadingModal";
 // import LoadingModal from "@/app/components/modals/LoadingModal";
 
 interface UserBoxProps {
@@ -28,8 +29,7 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   return (
     <>
       {isLoading && (
-        // <LoadingModal />
-        <p>Loading</p>
+        <LoadingModal />
       )}
       <div
         onClick={handleClick}
---
+++ app/users/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoadingModal from '../components/modals/LoadingModal'
+
+const loading = () => {
+  return (
+    <div>
+      <LoadingModal />
+    </div>
+  )
+}
+
+export default loading
